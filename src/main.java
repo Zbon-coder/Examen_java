5ca7a138d53b14b784805d40150470025dcbe01e
Zbon-coder <yves-noble.bonz@ism.edu.sn>
2026-04-09 21:55:12 +0100

    modification du fichier main par yves

--- src/main.java
+++ src/main.java
@@ -1,2 +1,27 @@
-public class main {
+import database.DatabaseConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/etudiant.fxml"));
+        primaryStage.setTitle("Gestion des Étudiants");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        DatabaseConnection.closeConnection();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
